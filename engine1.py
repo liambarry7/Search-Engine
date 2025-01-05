24aed923afc22b93214c9a9268c1a4a27ea35200
LB7 <liambarry406@yahoo.co.uk>
2025-01-05 00:14:42 +0000

    Work in progress
    Completed web scraping

--- engine1.py
+++ engine1.py
@@ -1,15 +1,25 @@
 import pandas
 import regex
-import requests
+
 from bs4 import BeautifulSoup, Comment
 
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+from nltk.stem import WordNetLemmatizer
+nltk.download('wordnet')
+from nltk.tokenize import word_tokenize
+nltk.download('punkt')
+
 def webScraper():
+    kgrams = []
+
     # get the name of html file from csv file
     videogame_details = pandas.read_csv('videogame-labels.csv')
     videogame_dict = videogame_details.to_dict(orient='records') # Convert CVS file from pandas data structure to dictionary
 
-    # for record in videogame_dict:
-    #     print(record)
+    for record in videogame_dict:
+        print(record)
 
     # get the url from each record reading to scrape, add to list
     list_of_urls = []
@@ -17,7 +27,10 @@ def webScraper():
         url = record['url']
         list_of_urls.append(url)
 
-    print(list_of_urls)
+    # print(list_of_urls)
+
+    # Dictionary to store all tokens from one document
+    list_of_document_tokens = []
 
     # beautiful soup
     for url in list_of_urls:
@@ -26,43 +39,69 @@ def webScraper():
         simpleURL = regex.sub(pattern, "", url)
         # print(simpleURL)
 
-
-
         # r = requests.get('videogames/' + simpleURL)
         r = open('videogames/' + simpleURL, 'r')
         soup = BeautifulSoup(r.read(), 'html5lib')
         soup.prettify()
         # print(soup)
-        #
-        # comments = soup.find_all(string=lambda text: isinstance(text, Comment))
-
-        # Loop through each comment to find the comment that includes 'DESCRIPTION'
-        # for comment in comments:
-        #     if "DESCRIPTION" in comment:
-        #
-        #         # Extract all content between the comments
-        #         next_sibling = comment.find_next_sibling()
-        #         text_content = []  # List to contain all the text after the comment
-        #
-        #         while next_sibling and not isinstance(next_sibling, Comment):
-        #             if next_sibling.name == 'p':
-        #                 text_content.append(next_sibling.get_text())
-        #             elif isinstance(next_sibling, str) and next_sibling.startswith('"') and next_sibling.endswith('"'):
-        #                 text_content.append(next_sibling)
-        #
-        #             next_sibling = next_sibling.next_sibling
-        #
-        #         print(text_content)
-        #
-        #         game_info = ''.join(str(item) for item in text_content)  # Combine each of the strings in the list text_content into one string
-        #         game_info.strip()  # Clean up the string by removing any leading or trailing whitespace characters (e.g. space, newline and tabs)
-        #         # print(game_info)
-        #         break  # Ignore any content after closing /DESCRIPTION comment
-
-        # break
 
 
+        # --------------------------------------------
+        # Get game title for kgram/raw name
+        raw_title = soup.find_all("title")
+        game_title = regex.sub(r"GameSpy: ", "", raw_title[0].get_text())
+        kgrams.append(game_title)
+        # print(game_title)
+        # --------------------------------------------
 
+        # Remove css and js from scraped html
+        for data in soup(['style', 'script']):
+            data.decompose()
+
+        # Get all text from under the content id
+        data = soup.find_all(id='content')
+        refined_data = str(data[0].get_text())
+        pattern = regex.compile(r"\s+")
+        simplified_data = regex.sub(pattern, " ", refined_data)
+        # print(simplified_data)
+
+        # remove punctuation and lowercase
+        removed_punctation = regex.sub(r"[^\w\s]", "", simplified_data)
+        removed_punctation = removed_punctation.lower()
+        # print(removed_punctation)
+
+        # Get stopwords
+        # nltk.download('stopwords')
+        stopwords_list = set(stopwords.words('English'))
+        # print(stopwords_list)
+
+        # Tokenise
+        # nltk.download('punkt')
+        tokens = word_tokenize(removed_punctation)
+
+        tokenised_words = []
+        for word in tokens:
+            if word not in stopwords_list:
+                tokenised_words.append(word)
+
+        # print(tokenised_words)
+
+        # Lemmatize
+        # nltk.download('wordnet')
+        lm = WordNetLemmatizer()
+        lemmatized_words = []
+
+        for word in tokenised_words:
+            lemmatized_words.append(lm.lemmatize(word))
+
+        # print(lemmatized_words)
+
+        vg_tokens = {"Name": game_title, "Tokens": lemmatized_words}
+        print(vg_tokens)
+        list_of_document_tokens.append(vg_tokens)
+
+    for w in list_of_document_tokens:
+        print(w)
 
 
 def getQuery():
