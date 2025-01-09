501df130e1711991d9cead8a654d4110ac83c218
LB7 <liambarry406@yahoo.co.uk>
2025-01-09 19:48:15 +0000

    Work in progress
    added single term and multi term td-idf scoring methods

--- test2.py
+++ test2.py
@@ -1,3 +1,5 @@
+import math
+
 import pandas
 import regex
 
@@ -18,8 +20,11 @@ def webScraper():
     videogame_details = pandas.read_csv('videogame-labels.csv')
     videogame_dict = videogame_details.to_dict(orient='records') # Convert CVS file from pandas data structure to dictionary
 
-    for record in videogame_dict:
-        print(record)
+    # count = 0
+    # for record in videogame_dict:
+    #     print(record)
+    #     count += 1
+    # print(count)
 
     # get the url from each record reading to scrape, add to list
     list_of_urls = []
@@ -78,13 +83,27 @@ def webScraper():
         lemmatized_words = lemmatizer(tokened_removed_stopwords)
 
         vg_tokens = {"Name": game_title, "Tokens": lemmatized_words}
-        print(vg_tokens)
         list_of_document_tokens.append(vg_tokens)
 
-    for w in list_of_document_tokens:
-        print(w)
 
+    # ---- testing ----
+    # for w in list_of_document_tokens:
+    #     print(w)
 
+    # testTokens = list_of_document_tokens[0]["Tokens"]
+    # print (testTokens)
+    # x = term_frequency("hackgu", testTokens)
+    # print ("tf ", x)
+    #
+    # print(inverse_document_frequency("wwe", list_of_document_tokens))
+
+    # tfidf_list = tf_idf("ICO", list_of_document_tokens)
+    # for i in tfidf_list:
+    #     print(i)
+
+    multi = multi_term_tdidf("wwe raw soldier", list_of_document_tokens)
+    for i in multi:
+        print(i)
 
 def getQuery():
     query = input("Please enter your query: ")
@@ -137,10 +156,102 @@ def lemmatizer(tokens):
 def stemming(tokens):
     print("Stemming")
 
+def term_frequency(term, doc_tokens):
+    # if no term in doc, score = 0
+    # tf = 1 + math.log(tf)
+
+    frequency = 0
+    for i in doc_tokens:
+        if term == i:
+            frequency += 1
+
+    if (frequency > 0):
+        tf = 1 + math.log(frequency, 10) # log x, base 10
+        return tf
+
+    else:
+        return 0
+
+def inverse_document_frequency(term, vg_list):
+    # idf = math.log(n / df)
+    # n = collection size
+    # df = no of docs term is in
+    # vg list = dictionary of each game's tokens
+
+    # print("\n\n")
+
+    # Basic df check
+    document_frequency = 0
+    for vg in vg_list:
+        for token in vg["Tokens"]:
+            if token == term:
+                document_frequency += 1
+                break # Break inner loop as term already found once in doc
+        else:
+            continue # Continue until term occurs or not found
+
+    collection_size = len(vg_list)
+
+    # Handle if term does not appear in any document
+    if document_frequency == 0:
+        return 0
+    else:
+        idf = math.log((collection_size / document_frequency), 10)
+        return idf
+
+def tf_idf(term, vg_list):
+    # tf * idf
+    print("\n")
+
+    # idf = inverse_document_frequency(term, vg_list)
+    # print(idf)
+    vg_tfidf = []
+
+    for i in range(len(vg_list)):
+        # term_dict = {"doc name": vg_list[i]["Name"], "tf": term_frequency(term, vg_list[i]["Tokens"])}
+        # print(term_dict)
+        tfidf = term_frequency(term, vg_list[i]["Tokens"]) * inverse_document_frequency(term, vg_list)
+        # print(tfidf)
+
+        doc_tfidf = {"Doc name": vg_list[i]["Name"], "tf-idf": tfidf}
+        vg_tfidf.append(doc_tfidf)
+
+    # returns list of each doc's tf-idf of the term
+    return vg_tfidf
+
+def multi_term_tdidf(query, vg_list):
+    query_terms = query.split(" ")
+    print(query_terms)
+
+    docs_tfidf = []
+
+    for i in range(len(vg_list)):
+
+        term_tdidf_list = []
+
+        for j in range(len(query_terms)):
+            tfidf = term_frequency(query_terms[j], vg_list[i]["Tokens"]) * inverse_document_frequency(query_terms[j], vg_list)
+            # print(tfidf)
+
+            t = {"Term": query_terms[j], "tf-idf": tfidf}
+            term_tdidf_list.append(t)
+
+        all_terms_tfidf = {"Doc name": vg_list[i]["Name"], "Query tf-idf": term_tdidf_list}
+        docs_tfidf.append(all_terms_tfidf)
+
+    return docs_tfidf
+
 def main():
     # query = getQuery()
     # print("Your query is:", query)
-    # webScraper()
-    csv_reader()
+    webScraper()
+    # csv_reader()
+
+    list = ['hackgu', 'vol', '1', 'rebirth', 'ps2', 'year', 'event', 'first', 'hack', 'installment', 'popular', 'dangerous', 'online', 'game', 'world', 'resurrected', 'haseo', 'terror', 'death', 'player', 'thrown', 'quest', 'revenge', 'search', 'mysterious', 'pk', 'player', 'killer', 'triedge', 'time', 'player', 'interact', 'real', 'virtual', 'world', 'fellow', 'world', 'player', 'various', 'chat', 'mail', 'fully', 'voiced', 'video', 'news', 'clip', 'together', 'haseos', 'virtual', 'partner', 'login', 'challenge', 'solve', 'mystery', 'new', 'environment', 'world', 'r2', 'second', 'series', 'hack', 'franchise', 'gu', 'feature', 'new', 'familiar', 'character', 'chapter', 'series', 'designed', 'distinct', 'first', 'hack', 'quadrilogy', 'email', 'friend', 'game', 'info', 'developer', 'cyberconnect2', 'publisher', 'bandai', 'genre', 'rpg', 'release', 'date', 'october', '24', '2006', 'esrb', 'teen', 'gamespy', 'score', 'read', 'review', 'cheat', 'walkthroughs', 'strategy', 'guide', 'cheat', 'hackgu', 'vol', '1', 'rebirth', 'article', 'interview', 'hackgu', '092105', 'roundtable', 'discussion', 'tgs', 'bandai', 'reveals', 'whats', 'next', 'hack', 'series', 'movie', 'hackgu', 'vol', '1', 'rebirth', 'trailer', '101906', 'trailer', 'introduce', 'u', 'battle', 'system', 'extraortinary', 'pkk', 'hackgu', 'vol', '1', 'rebirth', 'trailer', '092806', 'new', 'ability', 'shown', 'new', 'trailer', 'namco', 'bandais', 'upcoming', 'dungeon', 'crawler', 'new', 'hackgu', 'part', '1', 'rebirth', 'gameplay', 'video', '081406', 'second', 'series', 'hack', 'franchise', 'hackgu', 'part', '1', 'rebirth', 'new', 'trailer', '060806', 'get', 'new', 'sneak', 'peak', 'second', 'series', 'hack', 'franchise', 'hackgu', '050206', 'awesome', 'new', 'gameplay', 'footage', 'hackgu', 'part', '1', 'trailer', '091505', 'lengthy', 'cinematic', 'teaser', 'preview', 'hackgu', 'vol', '1', 'resurrection', '050206', 'popular', 'offline', 'mmo', 'get', 'new', 'start', 'fresh', 'series', 'hackgu', '091505', 'one', 'inventive', 'rpg', 'series', 'ps2', 'serf', 'tantalizing', 'detail', 'sequel', 'leaf', 'u', 'wanting', 'review', 'hackgu', '102706', 'offline', 'online', 'serial', 'rpg', 'return', 'hold', 'around', 'network', 'hackgu', 'vol', '1', 'ign', 'hackgu', 'vol', '1', 'cheat', 'ign', 'hackgu', 'vol', '1', 'gamespy', 'hackgu', 'vol', '1', 'gamestats', 'hackgu', 'vol', '1', 'cheat', 'ccg', 'hackgu', 'vol', '1', 'askmen']
+    # print(term_frequency("1", list))
+
+    # multi_term_tdidf("Hello World Fifa", list)
+
+
 
 main()
