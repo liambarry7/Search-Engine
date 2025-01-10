f2a02bf8406b317cb37c7075394b5d0a08a82fd4
LB7 <liambarry406@yahoo.co.uk>
2025-01-10 17:15:08 +0000

    Added vector space model (and cosine similarity) to return a result set of docs at precision 10.

--- EngineComponents.py
+++ EngineComponents.py
@@ -1,5 +1,6 @@
 import math
 
+import numpy
 import pandas
 import regex
 
@@ -89,6 +90,10 @@ def webScraper():
     # ---- testing ----
     # for w in list_of_document_tokens:
     #     print(w)
+    #     print(w["Tokens"][4])
+    #     if "ps2" in w["Tokens"]:
+    #         print("True")
+
 
     # testTokens = list_of_document_tokens[0]["Tokens"]
     # print (testTokens)
@@ -97,15 +102,39 @@ def webScraper():
     #
     # print(inverse_document_frequency("wwe", list_of_document_tokens))
 
-    # tfidf_list = tf_idf("ICO", list_of_document_tokens)
+    # tfidf_list = tfidf("ICO", list_of_document_tokens)
     # for i in tfidf_list:
     #     print(i)
 
-    multi = multi_term_tdidf("wwe raw soldier", list_of_document_tokens)
-    for i in multi:
+    # query = "soldier ps2 age"
+    query = "racing action sport"
+    multi = tdidf(query, list_of_document_tokens)
+    # # multi = tdidf("soldier", list_of_document_tokens)
+    # for i in multi:
+    #     print(i)
+
+
+    query_scores = query_tfidf(query, list_of_document_tokens)
+    for i in query_scores:
         print(i)
 
-    x = query_tfidf("wwe raw soldier", list_of_document_tokens)
+    # for i in range(len(multi)):
+    #     vector_space(query_scores["Vector"], multi[i]["Vector"]) # query tfidf scores, doc tfidf scores
+
+    dp_results = []
+    for q in multi:
+        # print(q["Name"])
+        # print(q)
+        x = vector_space(query_scores["Vector"], q["Vector"]) # query tfidf scores, doc tfidf scores
+        result_set = {"Name": q["Name"], "Dot product": x}
+        dp_results.append(result_set)
+
+    # for i in dp_results:
+    #     print(i)
+
+    cosine_similarity(dp_results)
+
+
 
 def getQuery():
     query = input("Please enter your query: ")
@@ -191,8 +220,8 @@ def inverse_document_frequency(term, vg_list):
             if token == term:
                 document_frequency += 1
                 break # Break inner loop as term already found once in doc
-        else:
-            continue # Continue until term occurs or not found
+        # else:
+        #     continue # Continue until term occurs or not found
 
     collection_size = len(vg_list)
 
@@ -203,29 +232,31 @@ def inverse_document_frequency(term, vg_list):
         idf = math.log((collection_size / document_frequency), 10)
         return idf
 
-def tf_idf(term, vg_list):
-    # tf * idf
-    print("\n")
+# def tf_idf(term, vg_list):
+#     # tf * idf
+#     print("\n")
+#
+#     # idf = inverse_document_frequency(term, vg_list)
+#     # print(idf)
+#     vg_tfidf = []
+#
+#     for i in range(len(vg_list)):
+#         # term_dict = {"doc name": vg_list[i]["Name"], "tf": term_frequency(term, vg_list[i]["Tokens"])}
+#         # print(term_dict)
+#         tfidf = term_frequency(term, vg_list[i]["Tokens"]) * inverse_document_frequency(term, vg_list)
+#         # print(tfidf)
+#
+#         doc_tfidf = {"Doc name": vg_list[i]["Name"], "tf-idf": tfidf}
+#         vg_tfidf.append(doc_tfidf)
+#
+#     # returns list of each doc's tf-idf of the term
+#     return vg_tfidf
 
-    # idf = inverse_document_frequency(term, vg_list)
-    # print(idf)
-    vg_tfidf = []
-
-    for i in range(len(vg_list)):
-        # term_dict = {"doc name": vg_list[i]["Name"], "tf": term_frequency(term, vg_list[i]["Tokens"])}
-        # print(term_dict)
-        tfidf = term_frequency(term, vg_list[i]["Tokens"]) * inverse_document_frequency(term, vg_list)
-        # print(tfidf)
-
-        doc_tfidf = {"Doc name": vg_list[i]["Name"], "tf-idf": tfidf}
-        vg_tfidf.append(doc_tfidf)
-
-    # returns list of each doc's tf-idf of the term
-    return vg_tfidf
-
-def multi_term_tdidf(query, vg_list):
+def tdidf(query, vg_list):
     query_terms = query.split(" ") # ------------ change to use tokeniser
+    # query_terms = tokeniser(query) # ------------ change to use tokeniser
     print(query_terms)
+
 
     docs_tfidf = []
 
@@ -242,44 +273,89 @@ def multi_term_tdidf(query, vg_list):
             term_tfidf_list.append(t)
             tfidf_list.append(tfidf)
 
-        all_terms_tfidf = {"Doc name": vg_list[i]["Name"], "Vector": tfidf_list, "Query-term tf-idf": term_tfidf_list}
+        all_terms_tfidf = {"Name": vg_list[i]["Name"], "Vector": tfidf_list, "Query-term tf-idf": term_tfidf_list}
         docs_tfidf.append(all_terms_tfidf)
 
     return docs_tfidf
 
-def vector_space():
-    print("Vectors")
-
 def query_tfidf(query, vg_list):
     # Create tf-idf for the query
-    query_tokens = tokeniser(query)
-    query_tfidf = []
+    query_tokens = tokeniser(remove_punc(query)) # Remove punctuation and make lower case to align with text data
+    term_tfidf = []
+    vector = []
 
     for term in query_tokens:
-        tf = term_frequency(term, query_tokens)
+        tf = term_frequency(term, query_tokens) # count number of times each term appears in query, divided by total number of terms in query
         idf = inverse_document_frequency(term, vg_list)
         tfidf = tf * idf
         t = {"Term": term, "tf-idf": tfidf}
-        query_tfidf.append(t)
+        term_tfidf.append(t)
+        vector.append(tfidf)
+
+    query_tfidf = {"Terms": term_tfidf, "Vector": vector}
 
     # print (query_tfidf)
+    # return query_tfidf
     return query_tfidf
 
+def vector_space(query_tfidf, vg_docs_tfidf):
+    # print("Vectors")
+    # Length normalised vectors allow for cosine comparison (dot product)
+    normalised_query_v = vector_len_normalisation(query_tfidf)
+    normalised_docs_v = vector_len_normalisation(vg_docs_tfidf)
+    # print("normalised query ", normalised_query_v)
+    # print("normalised doc ", normalised_docs_v)
 
+    # Dot product
+    dot_product = numpy.dot(normalised_query_v, normalised_docs_v)
+    # print("dp", dot_product)
+    return float(dot_product)
+
+
+
+def vector_len_normalisation(vector):
+    # Divide each term/component by the vector magnitude
+    # Normalised vector lengths are always 1
+    magnitude = math.sqrt(sum([i**2 for i in vector]))
+    # print("vector len normalisation ", magnitude)
+    if magnitude == 0:
+        # return an array of len(vector) of 0s for ease of dot product calculation
+        return [0 for l in vector]
+
+    else:
+        len_normalised = [x / magnitude for x in vector]
+    # print(len_normalised)
+    return len_normalised
+
+def cosine_similarity(dp_result_set):
+    # Sort out result set of the dot product calculation into desc order
+    # for i in dp_result_set:
+    #     print(i)
+    #
+    # print("\n\n\n")
+
+    sorted_rs = sorted(dp_result_set, key=lambda x:x['Dot product'], reverse = True)
+    # for i in sorted():
+    #     print(i)
+
+    # @10 precision
+    for i in range(1, 10):
+        print(sorted_rs[i])
 def main():
     # query = getQuery()
     # print("Your query is:", query)
     webScraper()
     # csv_reader()
 
-    list = ['hackgu', 'vol', '1', 'rebirth', 'ps2', 'year', 'event', 'first', 'hack', 'installment', 'popular', 'dangerous', 'online', 'game', 'world', 'resurrected', 'haseo', 'terror', 'death', 'player', 'thrown', 'quest', 'revenge', 'search', 'mysterious', 'pk', 'player', 'killer', 'triedge', 'time', 'player', 'interact', 'real', 'virtual', 'world', 'fellow', 'world', 'player', 'various', 'chat', 'mail', 'fully', 'voiced', 'video', 'news', 'clip', 'together', 'haseos', 'virtual', 'partner', 'login', 'challenge', 'solve', 'mystery', 'new', 'environment', 'world', 'r2', 'second', 'series', 'hack', 'franchise', 'gu', 'feature', 'new', 'familiar', 'character', 'chapter', 'series', 'designed', 'distinct', 'first', 'hack', 'quadrilogy', 'email', 'friend', 'game', 'info', 'developer', 'cyberconnect2', 'publisher', 'bandai', 'genre', 'rpg', 'release', 'date', 'october', '24', '2006', 'esrb', 'teen', 'gamespy', 'score', 'read', 'review', 'cheat', 'walkthroughs', 'strategy', 'guide', 'cheat', 'hackgu', 'vol', '1', 'rebirth', 'article', 'interview', 'hackgu', '092105', 'roundtable', 'discussion', 'tgs', 'bandai', 'reveals', 'whats', 'next', 'hack', 'series', 'movie', 'hackgu', 'vol', '1', 'rebirth', 'trailer', '101906', 'trailer', 'introduce', 'u', 'battle', 'system', 'extraortinary', 'pkk', 'hackgu', 'vol', '1', 'rebirth', 'trailer', '092806', 'new', 'ability', 'shown', 'new', 'trailer', 'namco', 'bandais', 'upcoming', 'dungeon', 'crawler', 'new', 'hackgu', 'part', '1', 'rebirth', 'gameplay', 'video', '081406', 'second', 'series', 'hack', 'franchise', 'hackgu', 'part', '1', 'rebirth', 'new', 'trailer', '060806', 'get', 'new', 'sneak', 'peak', 'second', 'series', 'hack', 'franchise', 'hackgu', '050206', 'awesome', 'new', 'gameplay', 'footage', 'hackgu', 'part', '1', 'trailer', '091505', 'lengthy', 'cinematic', 'teaser', 'preview', 'hackgu', 'vol', '1', 'resurrection', '050206', 'popular', 'offline', 'mmo', 'get', 'new', 'start', 'fresh', 'series', 'hackgu', '091505', 'one', 'inventive', 'rpg', 'series', 'ps2', 'serf', 'tantalizing', 'detail', 'sequel', 'leaf', 'u', 'wanting', 'review', 'hackgu', '102706', 'offline', 'online', 'serial', 'rpg', 'return', 'hold', 'around', 'network', 'hackgu', 'vol', '1', 'ign', 'hackgu', 'vol', '1', 'cheat', 'ign', 'hackgu', 'vol', '1', 'gamespy', 'hackgu', 'vol', '1', 'gamestats', 'hackgu', 'vol', '1', 'cheat', 'ccg', 'hackgu', 'vol', '1', 'askmen']
+    # list = ['hackgu', 'vol', '1', 'rebirth', 'ps2', 'year', 'event', 'first', 'hack', 'installment', 'popular', 'dangerous', 'online', 'game', 'world', 'resurrected', 'haseo', 'terror', 'death', 'player', 'thrown', 'quest', 'revenge', 'search', 'mysterious', 'pk', 'player', 'killer', 'triedge', 'time', 'player', 'interact', 'real', 'virtual', 'world', 'fellow', 'world', 'player', 'various', 'chat', 'mail', 'fully', 'voiced', 'video', 'news', 'clip', 'together', 'haseos', 'virtual', 'partner', 'login', 'challenge', 'solve', 'mystery', 'new', 'environment', 'world', 'r2', 'second', 'series', 'hack', 'franchise', 'gu', 'feature', 'new', 'familiar', 'character', 'chapter', 'series', 'designed', 'distinct', 'first', 'hack', 'quadrilogy', 'email', 'friend', 'game', 'info', 'developer', 'cyberconnect2', 'publisher', 'bandai', 'genre', 'rpg', 'release', 'date', 'october', '24', '2006', 'esrb', 'teen', 'gamespy', 'score', 'read', 'review', 'cheat', 'walkthroughs', 'strategy', 'guide', 'cheat', 'hackgu', 'vol', '1', 'rebirth', 'article', 'interview', 'hackgu', '092105', 'roundtable', 'discussion', 'tgs', 'bandai', 'reveals', 'whats', 'next', 'hack', 'series', 'movie', 'hackgu', 'vol', '1', 'rebirth', 'trailer', '101906', 'trailer', 'introduce', 'u', 'battle', 'system', 'extraortinary', 'pkk', 'hackgu', 'vol', '1', 'rebirth', 'trailer', '092806', 'new', 'ability', 'shown', 'new', 'trailer', 'namco', 'bandais', 'upcoming', 'dungeon', 'crawler', 'new', 'hackgu', 'part', '1', 'rebirth', 'gameplay', 'video', '081406', 'second', 'series', 'hack', 'franchise', 'hackgu', 'part', '1', 'rebirth', 'new', 'trailer', '060806', 'get', 'new', 'sneak', 'peak', 'second', 'series', 'hack', 'franchise', 'hackgu', '050206', 'awesome', 'new', 'gameplay', 'footage', 'hackgu', 'part', '1', 'trailer', '091505', 'lengthy', 'cinematic', 'teaser', 'preview', 'hackgu', 'vol', '1', 'resurrection', '050206', 'popular', 'offline', 'mmo', 'get', 'new', 'start', 'fresh', 'series', 'hackgu', '091505', 'one', 'inventive', 'rpg', 'series', 'ps2', 'serf', 'tantalizing', 'detail', 'sequel', 'leaf', 'u', 'wanting', 'review', 'hackgu', '102706', 'offline', 'online', 'serial', 'rpg', 'return', 'hold', 'around', 'network', 'hackgu', 'vol', '1', 'ign', 'hackgu', 'vol', '1', 'cheat', 'ign', 'hackgu', 'vol', '1', 'gamespy', 'hackgu', 'vol', '1', 'gamestats', 'hackgu', 'vol', '1', 'cheat', 'ccg', 'hackgu', 'vol', '1', 'askmen']
     # print(term_frequency("1", list))
 
     # multi_term_tdidf("Hello World Fifa", list)
 
+    # vector_len_normalisation([5,4,3])
+    # vector_space([5,4,3], [9,8,7])
+
     ''' --- TO DO ---
-        - Get query tf-idf - check it works
-        - check if multi tfidf and single term tfidf are the same (mutli handles singular queries)
         - Use Vector Space model
         - Cosine? 
             - both are equal to the dot product slide 50
@@ -289,6 +365,7 @@ def main():
         - Read in html files differently
         - CVS look up method for result comparisons
         - Build different engines utilising different components
+        - need to lem/stem the query to get to the same simplification as the docment text
     '''
 
 
